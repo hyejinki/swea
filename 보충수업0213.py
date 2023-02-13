0536f1438b77231ef4f14d0d53846c45742d8bc0
hyejinki <zlagg23@gmail.com>
2023-02-13 19:59:09 +0900

    8:00

--- 보충수업0213.py
+++ 보충수업0213.py
@@ -53,29 +53,60 @@
 
 
 # 파스칼의 삼각형
-# 점화식
-# 첫 번째 열, 마지막 열은 다 1이다. 
-# a21 = a10 + a11
-# a31 = a20 + a21
-# arc = a(r-1)(c-1) + a(r-1)c
-
-T = int(input())
-for tc in range(1, T+1):
-    N = int(input())
-    print(f'#{tc}')
-    # 파스칼의 삼각형 구하기
-    tri = [[0] * r for r in range(1, N+1)]
 
 
-    # 한줄씩 구하면서 출력
-    # 이전 행에서 사용했던 결과를 이용해서 현재 행을 구하기
-    for r in range(N):
-        for c in range(r + 1):
-            if c == 0:                  # 맨 왼쪽 열
-                tri[r][c] = 1
-            elif r == c:                # 대각선
-                tri[r][c] = 1
-            else:
-                tri[r][c] = tri[r-1][c-1] + tri[r-1][c]
+# # 점화식
+# # 첫 번째 열, 마지막 열은 다 1이다. 
+# # a21 = a10 + a11
+# # a31 = a20 + a21
+# # arc = a(r-1)(c-1) + a(r-1)c
+
+# T = int(input())
+# for tc in range(1, T+1):
+#     N = int(input())
+#     print(f'#{tc}')
+#     # 파스칼의 삼각형 구하기
+#     tri = [[0] * r for r in range(1, N+1)]
+
+
+#     # 한줄씩 구하면서 출력
+#     # 이전 행에서 사용했던 결과를 이용해서 현재 행을 구하기
+#     for r in range(N):
+#         for c in range(r + 1):
+#             if c == 0:                  # 맨 왼쪽 열
+#                 tri[r][c] = 1
+#             elif r == c:                # 대각선
+#                 tri[r][c] = 1
+#             else:
+#                 tri[r][c] = tri[r-1][c-1] + tri[r-1][c]
         
-        print(*tri[r])
+#         print(*tri[r])
+
+
+
+# 스택으로 풀기
+T = int(input())
+
+for tc in range(1, T+1):
+    n = int(input())
+
+    # 첫번째 줄
+    tri = [1]
+    print(f'{tc}')
+    print(*tri)
+    # 두 번째 줄부터 스택을 사용해서 삼각형 만들기
+    for i in range(n - 1):
+        # 현재줄의 맨처음과 끝에 있는 1은 더할 게 없으니 0으로 채워준다.
+        stack = [0] + tri + [0]
+        tri = [] # 해당 줄에서 사용할 숫자를 저장할 리스트
+
+        # pop()을 하면 최근에 사용했던 값을 가져올 수 있다.
+        num = stack.pop()   # 자신의 오른쪽 위 숫자 가져오기
+
+        while stack:
+            num2 = stack.pop()      #왼쪽 위 숫자 가져오기
+            tri.append(num + num2) 
+
+            num = num2  # 자신의 오른쪽 숫자를 왼쪽 숫자로 바꿔준다.
+
+        print(*tri)      
